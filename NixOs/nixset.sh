414ce46af7d235e06e701204d7ee00cd9691bdae
dwirx <bangundwir@gmail.com>
2024-03-04 12:30:52 +0700

    update

---
+++ NixOs/nixset.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Selamat datang, Hendra!"
+
+# Menampilkan pilihan untuk pengunduhan
+echo "Silakan pilih opsi pengunduhan:"
+echo "1. Hardware Configuration"
+echo "2. Configuration"
+
+# Membaca input dari pengguna
+read -p "Masukkan nomor pilihan (1 atau 2): " option
+
+# Menentukan URL berdasarkan pilihan
+case $option in
+  1)
+    url="https://raw.githubusercontent.com/dwirx/NixOs/master/hardware-configuration.nix"
+    ;;
+  2)
+    url="https://raw.githubusercontent.com/dwirx/NixOs/master/configuration.nix"
+    ;;
+  *)
+    echo "Pilihan tidak valid. Keluar."
+    exit 1
+    ;;
+esac
+
+# Menentukan nama file untuk disimpan
+filename=$(basename $url)
+
+# Mengunduh file dengan curl
+curl -O $url
+
+echo "Pengunduhan selesai. File disimpan sebagai: $filename"
+
